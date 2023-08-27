e2361897c3c3e8c53c2b50ddd0ec092fdaa305af
Mohamed El Gedawy <Mohamed.hgedawy@gmail.com>
2023-08-28 01:48:30 +0300

    Initial Project Add .gitIgnore

---
+++ YassirMovies/YassirMovies/YassirMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirMoviesApp.swift
+//  YassirMovies
+//
+//  Created by Mohamed El Gedawy on 27/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct YassirMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
